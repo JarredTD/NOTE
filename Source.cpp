b2fd577fe07663c0ea6a7c9425635f2504f1c45c
JarredTD <jarred.desrosiers@yahoo.com>
2022-06-17 00:51:33 -0400

    Started ADD

--- Source.cpp
+++ Source.cpp
@@ -15,13 +15,6 @@ int main(int argc, char** argv)
 				note.name = argv[2];
 				note.message = argv[3];
 
-
-				/*std::cin.ignore();
-				std::getline(std::cin, input);
-				note = Create_Note(input);
-
-				if (note.name == FAIL) { return -1; }*/
-
 				std::cout << note.name << ": " << note.message << std::endl;
 				break;
 
@@ -31,41 +24,4 @@ int main(int argc, char** argv)
 	}
 
 	return 0;
-}
-
-
-std::string Find_Name(std::string input)
-{
-	std::string name;
-
-	std::size_t start_point = 0;
-	std::size_t end_point = input.find('=');
-	
-	if (end_point == std::string::npos) { return FAIL; }
-
-	for (end_point; input[end_point] != ' '; end_point--)
-	for (start_point; input[start_point] != ' '; start_point++){}
-
-	name = input.substr(start_point, end_point);
-
-	return name;
-}
-
-std::string Find_Message(std::string input)
-{
-	std::string message;
-
-	return "f";
-}
-
-Note Create_Note(std::string input)
-{
-	Note new_note;
-	
-	new_note.name = Find_Name(input);
-	if (new_note.name == FAIL) { return new_note; }
-
-	new_note.message = Find_Message(input);
-
-	return new_note;
 }
--- Source.h
+++ Source.h
@@ -5,22 +5,10 @@
 
 const char COMMAND_START = '-';
 const char ADD = 'a';
-const std::string FAIL = " ";
 
-const int MESSAGE_SIZE = 51;
 
 struct Note
 {
 	std::string name;
 	std::string message;
 };
-
-// Creates a new node structure and returns a ptr to it. 
-// Takes in a message and a name. Creates note with said inputs.
-Note Create_Note(std::string);
-
-// Takes in a string and finds "=". Will copy anything but whitespace until the "=" is found.
-std::string Find_Name(std::string);
-
-// takes in a string and finds "=". Will copy anything except the first whitspace after the "=" until the null terminator ("\0").
-std::string Find_Message(std::string);
